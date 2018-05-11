1a31ee7f93461bf0704d9c14a7ee013124b7eea8
KeanuGo <keanuguanezogo@yahoo.com>
2018-05-11 21:15:27 +0800

    Update PCB.java

--- PCB.java
+++ PCB.java
@@ -1,5 +1,5 @@
 public class PCB{
-	protected int arrival_time, burst_time, pid, wait_time, turnarround_time, priority, completion_time, origburst_time;
+	protected int arrival_time, burst_time, pid, priority, origburst_time;
 	public boolean finish= false;
 	public PCB(int pid, int arrival_time, int burst_time, int priority){
 		this.pid= pid;
@@ -17,18 +17,6 @@ public class PCB{
 	public void setBurst_time(int burst_time){
 		this.burst_time = burst_time;
 	}
-	public void setCompletion_time(int completion_time){
-		this.completion_time = completion_time;
-	}
-	public int getCompletion_time(){
-		return completion_time;
-	}
-	public void setTurnaround_time(int turnarround_time){
-		this.turnarround_time = turnarround_time;
-	}
-	public int getTurnaround_time(){
-		return turnarround_time;
-	}
 	public int getArrival_time(){
 		return arrival_time;
 	}
@@ -37,12 +25,6 @@ public class PCB{
     }
     public void setPid(int pid) {
         this.pid = pid;
-    }
-	public int getWait_time(){
-		return wait_time;
-	}
-	public void setWait_time(int wait_time) {
-        this.wait_time = wait_time;
     }
 	public int getPriority(){
 		return priority;
